18c9b21c0fda73198617ac1439cd3d3a05282d54
Amy Huxford <amyhuxford@wifi-172-23-168-111.uoa-wifi.auckland.ac.nz>
2025-07-30 14:08:35 +1200

    adding testing files to backend

---
+++ backend/dataprocessing/test_framer_timing.py
@@ -0,0 +1,22 @@
+import time
+import os
+from framer3 import framer
+
+# Path to the videos folder
+video_dir = "./videos"  # Updated to point to the videos folder
+video_extensions = ('.mp4', '.avi', '.mov', '.mkv')
+
+# Get all video files in the directory
+video_files = [os.path.join(video_dir, f) for f in os.listdir(video_dir) if f.endswith(video_extensions)]
+
+# Run framer on each video and time how long it takes
+for video_path in video_files:
+    video_name = os.path.basename(video_path)
+    print(f"Processing: {video_name}")
+
+    start = time.time()
+    framer(video_path)
+    end = time.time()
+
+    duration = end - start
+    print(f"{video_name} took {duration:.2f} seconds\n")
